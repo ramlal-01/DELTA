f04136db64ac0e99186e1c718ee95b641bfec91a
ramlal-01 <ramlal0801@gmail.com>
2024-10-12 13:48:53 +0530

    Part 2 Done

--- JS/JS (Part 2)/app.js
+++ JS/JS (Part 2)/app.js
@@ -1,5 +1,29 @@
-console.log("Hello World");
-console.log("JavaScript Started");
-let a= 10;
-let b=20;
-console.log(a+b);
+// console.log("Hello World");
+// console.log("JavaScript Started");
+// let a= 10;
+// let b=20;
+// console.log(`The sum is ${a+b}`);
+// console.log(1=='1');
+// console.log(1==='1');
+
+// Operators Done
+// Conditional Statement Done
+
+// truthy and falsy value
+
+// if(NaN){
+//     console.log("value is true");
+// }else{
+//     console.log("value is false");
+// }
+
+// alert("Do Not Open!!");
+// console.log("This is a simple error.");
+// console.error("This is an Error message.");
+// console.warn("This is a warning msg.");
+
+let firstName= prompt("Enter Your First Name:");
+let lastName= prompt("Enter Your Last Name:");
+let msg="Welcome"+" "+firstName+" "+lastName+"!";
+alert(msg);
+// console.log("Welcome",firstName,lastName,"!");
