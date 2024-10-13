2ef89443d0611e6e00cac80085b8ce6384dff294
ramlal-01 <ramlal0801@gmail.com>
2024-10-13 23:54:56 +0530

    Part 3 Done

---
+++ JS/JS (Part 3)/app.js
@@ -0,0 +1,25 @@
+// let password= prompt("Enter Your Password :");
+// console.log(password.trim());
+// console.log(password.toUpperCase());
+// console.log(password.toLowerCase());
+// let msg= "ilovecoding";
+// console.log(msg.indexOf("q"));
+// console.log(password.trim().toLowerCase());
+// console.log(password[5]);
+
+// Arrays
+
+// let info =["ram",23,3.2];
+// console.log(info);
+// info[10]="Mathura";
+
+// let cars= ["nexa", "ferrari", "bmw"];
+// console.log(cars.push("maruti"));
+// console.log(cars.pop());
+// console.log(cars.unshift("audi"));
+// console.log(cars.shift());
+
+let colors =[ "red", "yellow", "blue", "orange","pink","white"];
+colors.splice(4); /*will work as slice */
+colors.splice(0,1);
+colors.splice(0,1,"black","grey");
