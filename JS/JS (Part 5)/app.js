cd8cda9c6c5bfe39ba3a7fc6d96ee9bf74ab2046
ramlal-01 <ramlal0801@gmail.com>
2024-10-15 09:37:04 +0530

    Part 5 Object literals

---
+++ JS/JS (Part 5)/app.js
@@ -0,0 +1,16 @@
+// object literals
+let info = {name:"ram lal", sec:"B", age:19,city:"Mathura"};
+const items={
+    price:99.99,
+    discount:49,
+    color:["black","white","golden"]
+};
+
+// JS automatically converts keys into string , null and true are string here not keyword.
+let obj={
+    1:'a',
+    2:'b',
+    true:'c',
+    null:'d',
+    undefined:'e'
+};   
