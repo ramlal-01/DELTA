a89fbdd1f36addb3cf5ac3f8b11a251acc6f5829
ramlal-01 <ramlal0801@gmail.com>
2024-10-15 06:21:25 +0530

    Part 4

--- JS/JS (Part 4)/app.js
+++ JS/JS (Part 4)/app.js
@@ -60,3 +60,13 @@ while(true){
     req= prompt("please enter your request");
 
 }
+
+/*let number=287152;
+let sum=0;
+let copy=number;
+while(copy>0){
+    digit=copy%10;
+    sum+=digit;
+    copy=Math.floor(copy/10);
+}
+console.log(sum);*/
