938e9bc38799e0b49143552db1768e65409b67f3
ramlal-01 <ramlal0801@gmail.com>
2024-11-08 07:54:20 +0530

    JS DOM

---
+++ JS/JS (Part 9)/app.js
@@ -0,0 +1,14 @@
+// let smallImages = document.getElementsByClassName("oldImg");
+// for(let i=0 ; i< smallImages.length ; i++){
+//     smallImages[i].src = "assets/spiderman_img.png";
+//     console.log(` value of image no. ${i} is changed.`);
+// }
+
+// document.getElementsByTagName("p");
+// document.getElementsByTagName("p")[1].innerText = "abc";
+
+// console.dir(document.querySelector("h1"));
+// console.dir(document.querySelector("#description"));
+// console.dir(document.querySelector(".oldImg"));
+// console.dir(document.querySelector("div a"));
+console.dir(document.querySelectorAll("div a"));
